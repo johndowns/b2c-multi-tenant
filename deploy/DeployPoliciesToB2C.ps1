2eef9064c4c20e55821223c518e0aaddb04d2a33
John Downs <john.downs@microsoft.com>
2020-12-16 21:31:01 +1300

    .

--- deploy/DeployPoliciesToB2C.ps1
+++ deploy/DeployPoliciesToB2C.ps1
@@ -6,8 +6,6 @@ Param(
     [Parameter(Mandatory = $true)][string]$PathToFiles
 )
 
-. deploy/DeployToB2C.ps1
-
 $policyIds = @(
     'TrustFrameworkBase',
     'TrustFrameworkExtensions',
@@ -16,9 +14,40 @@ $policyIds = @(
     'SignUpOrSignIn',
     'SignUpSignInMultiTenantAAD')
 
-foreach ($policyId in $policyIds)
-{
-    Write-Host $policyId
-    $pathToFile = Join-Path $PathToFiles "$policyId.xml"
-    DeployToB2C -ClientId $ClientId -ClientSecret $ClientSecret -TenantId $TenantId -PolicyId $policyId -PathToFile $pathToFile
+try {
+    $body = @{grant_type = "client_credentials"; scope = "https://graph.microsoft.com/.default"; client_id = $ClientID; client_secret = $ClientSecret }
+
+    $response = Invoke-RestMethod -Uri https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token -Method Post -Body $body
+    $token = $response.access_token
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", 'application/xml')
+    $headers.Add("Authorization", 'Bearer ' + $token)
+
+    foreach ($policyId in $policyIds)
+    {
+        $graphuri = 'https://graph.microsoft.com/beta/trustframework/policies/' + $policyId + '/$value'
+        $pathToFile = Join-Path $PathToFiles "$policyId.xml"
+        $policyContent = Get-Content $PathToFile
+        $response = Invoke-RestMethod -Uri $graphuri -Method Put -Body $policyContent -Headers $headers
+
+        Write-Host "Policy" $PolicyId "uploaded successfully."
+    }
 }
+catch {
+    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__
+
+    $_
+
+    $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
+    $streamReader.BaseStream.Position = 0
+    $streamReader.DiscardBufferedData()
+    $errResp = $streamReader.ReadToEnd()
+    $streamReader.Close()
+
+    $ErrResp
+
+    exit 1
+}
+
+exit 0
